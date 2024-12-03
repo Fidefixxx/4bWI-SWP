1f2c6d2f453525926dce92462fcaa11d9c1cb5b1
Fidefixxx <p1.gsteu@gmail.com>
2024-12-03 09:09:50 +0100

    11 JS Arrays Aufgabe 5 DONE

--- 2JS/11 Arrays Aufgabe 5/script.js
+++ 2JS/11 Arrays Aufgabe 5/script.js
@@ -1,2 +1,13 @@
 //Berechne die Summe der geraden durch die Summe der ungeraden Zahlen
-const data2 = "1,2,3,4,5,6,7,8,9,10,11,12"
+data = []
+for (let i = 0; i < Math.random()*100; i++) {
+    data.push(Math.floor(Math.random() * 100))
+}
+let sumEven = 0
+let sumOdd = 0
+for (let i = 0; i < data.length; i++) {
+    if (data[i] % 2 === 0) sumEven += data[i]
+    else sumOdd += data[i]
+}
+console.log(data)
+console.log(sumEven / sumOdd)
