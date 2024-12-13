f5c91566d74c25dd94f9025f4b8c913f1980f318
Fidefixxx <p1.gsteu@gmail.com>
2024-12-13 15:42:57 +0100

    01 JS HTML Rechner DONE

--- 3JS_HTML/01 Zugriff HTML/script.js
+++ 3JS_HTML/01 Zugriff HTML/script.js
@@ -1,17 +1,27 @@
 function ButtonClick() {
-    let input1 = parseInt(document.getElementById("input1").value);
-    let input2 = parseInt(document.getElementById("input2").value);
+    let input1 = parseFloat(document.getElementById("input1").value);
+    let input2 = parseFloat(document.getElementById("input2").value);
     let result;
+    let operator;
 
     if (document.getElementById("add").checked) {
-        result = input1 + input2;
+        result = (input1 + input2).toFixed(5);
+        operator = "+";
     } else if (document.getElementById("subtract").checked) {
-        result = input1 - input2;
+        result = (input1 - input2).toFixed(5);
+        operator = "-";
     } else if (document.getElementById("multiply").checked) {
-        result = input1 * input2;
+        result = (input1 * input2).toFixed(5);
+        operator = "×";
     } else if (document.getElementById("divide").checked) {
-        result = input1 / input2;
+        result = (input1 / input2).toFixed(5);
+        operator = "÷";
     }
 
-    document.getElementById("result").innerHTML = result;
+    result = parseFloat(result);
+
+    document.getElementById("calculation").innerHTML = input1 + " " + operator + " " + input2 + " = " + result;
 }
+
+
+
