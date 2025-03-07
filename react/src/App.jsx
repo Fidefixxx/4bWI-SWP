583c0870ce65c4e0cfb25762351336d970ca9266
Fidefixxx <p1.gsteu@gmail.com>
2025-03-07 16:37:40 +0100

    02 REACT Komponenten DONE

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/card'
+import List from './components/List'
+import Button from './components/button'
+import AvatarCard from './components/AvatarCard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,11 +20,23 @@ function App() {
       <h6 className='bg-emerald-600 border-10 border-double border-green-200 mx-10 p-3'>test6</h6>
       <div className='flex'>
         <Card name="John" job="Developer" desc="Frontend Developer"/>
-        <Card name="John" job="Developer" desc="Frontend Developer"/>
-        <Card name="John" job="Developer" desc="Frontend Developer"/>
-        <Card name="John" job="Developer" desc="Frontend Developer"/>
-        <Card name="John" job="Developer" desc="Frontend Developer"/>
-        <Card name="John" job="Developer" desc="Frontend Developer"/>
+        <Card name="Ömer" job="Teacher" desc="School-Teacher "/>
+        <Card name="Cem" job="Manager" desc="Project Manager"/>
+        <Card name="Matteo" job="CEO" desc="CEO of Facebook"/>
+        <Card name="Turancan" job="Cook" desc="Cook in Dornbirn"/>
+        <Card name="Zeki" job="Mechanic" desc="Mechanic for VW"/>
+      </div>
+      <div>
+        <List length={4} data={["Lisdsadfhisbfphadsbfphbfdphafbphbpjfbasdfhbtitem", "Nocheins", "und des dritte", "lorem impsum dings da"]}/>
+      </div>
+      <div>
+        <Button text="djabfjhfbphewbfphqewbfhp"/>
+      </div>
+      <div class="flex">
+        <AvatarCard Name="Matteo" Surname="Kleber" Age="18" Profession="Student" Description="MK on top"/>
+        <AvatarCard Name="Peter" Surname="Gsteu" Age="18" Profession="Student" Description="peta"/>
+        <AvatarCard Name="Matthias" Surname="hagen" Age="19" Profession="Student" Description="Eishocky"/>
+        <AvatarCard Name="Valentin" Surname="Gächter" Age="18" Profession="Student" Description="vali <3"/>
       </div>
      </>
   )
---
+++ react/src/components/Button.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Button({text} ) {
+  return (
+    <button class="m-5 w-fit p-8 text-lg bg-blue-400  rounded-md outline-5 outline-blue outline-double hover:cursor-pointer hover:bg-blue-500 hover:outline-solid hover:outline-3">{text}</button>
+  )
+}
