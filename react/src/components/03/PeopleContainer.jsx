41ec5ccba20c21639faa7b667c94ae678cac34fa
Fidefixxx <p1.gsteu@gmail.com>
2025-03-14 16:20:48 +0100

    03 REACT Demo Project Cards 50% DONE

--- react/src/components/03/PeopleContainer.jsx
+++ react/src/components/03/PeopleContainer.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PersonCard from "./PersonCard";
 
 export default function PeopleContainer() {
+  useEffect(()=>{
+
+    fetch("https://67d4412fd2c7857431ed076b.mockapi.io/Person").then(res => res.json().then(data=>{
+      console.log(data);
+      people = data;
+
+    }))
+  },[])
   return (
     <div>
       <div>PeopleContainer</div>
-      <div>
-        <PersonCard name="Matteo" title="CEO" iconurl="https://picsum.photos/200"/>
+      <div class="grid grid-cols-2 gap-x-5 m-3 md:grid-cols-4 lg:grid-cols-6">
+        {people.map(element => {
+          return <div></div>
+
+        })}
       </div>
     </div>
   );
--- react/src/components/03/PersonCard.jsx
+++ react/src/components/03/PersonCard.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function PersonCard({name, title, iconurl}) {
   return (
-    <div class="flex items-center gap-4 p-4 m-2 border w-70">
-        <div>
-            <p class="text-3xl">{name}</p>
-            <p class="text-md">{title}</p>
+    <div class="flex items-center gap-4 m-2 border w-full rounded">
+        <div class="ml-2">
+            <p class="text-2xl">{name}</p>
+            <p class="text-sm italic">{title}</p>
         </div>
         <div className='w-full'>
-            <img class="object-cover rounded" src={iconurl} alt='' />
+            <img class="w-full h-full object-cover rounded" src={iconurl} alt='' />
         </div>
     </div>
   )
