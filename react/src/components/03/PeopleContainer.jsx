d786fbe03c0e018bef85ee3feec25810915cdf45
Fidefixxx <p1.gsteu@gmail.com>
2025-03-18 09:35:35 +0100

    03 REACT Demo Project Cards 60% DONE

--- react/src/components/03/PeopleContainer.jsx
+++ react/src/components/03/PeopleContainer.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import PersonCard from "./PersonCard";
 
 export default function PeopleContainer() {
-  useEffect(()=>{
+  const [people, setPeople] = useState([])
 
+  useEffect(()=>{
     fetch("https://67d4412fd2c7857431ed076b.mockapi.io/Person").then(res => res.json().then(data=>{
-      console.log(data);
-      people = data;
+      data.forEach(element => {
+        element.picture=`https://picsum.photos/id/${element.id}/200`
+      });
+      setPeople(data);
 
     }))
   },[])
@@ -15,8 +18,8 @@ export default function PeopleContainer() {
       <div>PeopleContainer</div>
       <div class="grid grid-cols-2 gap-x-5 m-3 md:grid-cols-4 lg:grid-cols-6">
         {people.map(element => {
-          return <div></div>
-
+          console.log(element)
+          return <PersonCard name={element.name} title={element.job} iconurl={element.picture}/> 
         })}
       </div>
     </div>
